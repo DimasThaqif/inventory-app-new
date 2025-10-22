ba54d1ff449e3cd5f13b2c49d200567e27e2fc39
dimasthaqif <dimasthaqif10@gmail.com>
2025-10-22 15:52:29 +0700

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = Item::all();
+
+        return inertia('Items/Index', [
+            'items' => $items,
+        ]);
+    }
 }
---
+++ resources/js/Pages/Items/Index.vue
@@ -0,0 +1,16 @@
+<script setup>
+    defineProps({
+        items: Array
+    });
+</script>
+
+<template>
+
+<div>
+    <h1>Item list</h1>
+    <ul>
+        <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ul>
+</div>
+
+</template>
