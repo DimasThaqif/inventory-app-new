9098b2491162dc60074d02b6065c6ca2246435c9
dimasthaqif <dimasthaqif10@gmail.com>
2025-10-22 16:46:49 +0700

    update

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -1,11 +1,50 @@
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
     defineProps({
         items: Array
-    });
+    })
 </script>
 
 <template>
-    <div>
-        <h1></h1>
-    </div>
+     <Head title="Items" />
+    
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Item List
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <table class="table-auto w-full mt-4 border-collapse border border-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr v-for="(item, index) in items" :key="index">
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ ++index }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ item.name }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ item.qty }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap"> edit | stock card</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
 </template>
