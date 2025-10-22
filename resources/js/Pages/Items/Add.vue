8031ad6b735f65713a6459bc8c5b57b4e2956600
dimasthaqif <dimasthaqif10@gmail.com>
2025-10-22 19:26:24 +0700

    update

---
+++ resources/js/Pages/Items/Add.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+</script>
+
+<template>
+     <Head title="Add Items" />
+    
+    <AuthenticatedLayout>
+         <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Add Item
+            </h2>
+        </template>
+    </AuthenticatedLayout>
+</template>
