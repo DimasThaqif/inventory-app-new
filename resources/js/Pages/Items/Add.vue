6a12190f533e062a14e2f6ee3507b1e9062fbf09
dimasthaqif <dimasthaqif10@gmail.com>
2025-10-23 21:22:25 +0700

    tes

--- resources/js/Pages/Items/Add.vue
+++ resources/js/Pages/Items/Add.vue
@@ -32,7 +32,7 @@ const submitForm = () => {
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="submitForm" action="/items" method="POST">
                             <div class="mb-4">
-                                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                                <label for="name" class="block text-sm font-medium text-gray-700">Namee</label>
                                 <input type="text" v-model="form.name" id="name" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required />
                             </div>
                             <div class="mb-4">
