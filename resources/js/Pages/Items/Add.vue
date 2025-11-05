ecd746063b6b006a66f694237b63d7a9c50c63bd
dimasthaqif <dimasthaqif10@gmail.com>
2025-11-05 23:36:40 +0700

    menambahkan alert full

--- resources/js/Pages/Items/Add.vue
+++ resources/js/Pages/Items/Add.vue
@@ -16,7 +16,7 @@ const submitForm = () => {
 
 <template>
      <Head title="Add Items" />
-    
+
     <AuthenticatedLayout>
          <template #header>
             <h2
@@ -34,10 +34,12 @@ const submitForm = () => {
                             <div class="mb-4">
                                 <label for="name" class="block text-sm font-medium text-gray-700">Namee</label>
                                 <input type="text" v-model="form.name" id="name" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required />
+                                <div class="bg-red-200 p-2 mt-2 rounded" v-if="form.errors.name">{{ form.errors.name }}</div>
                             </div>
                             <div class="mb-4">
                                 <label for="qty" class="block text-sm font-medium text-gray-700">Quantityy</label>
                                 <input type="number" v-model="form.qty" id="qty" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required />
+                                <div class="bg-red-200 p-2 mt-2 rounded" v-if="form.errors.qty">{{ form.errors.qty }}</div>
                             </div>
                             <div class="flex items-center justify-end">
                                 <primary-button>
